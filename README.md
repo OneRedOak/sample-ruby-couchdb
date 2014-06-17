2fdb2669b4113ac86c447fdcc067bcb36661336d
Patrick Ellis <oneredoak@gmail.com>
2014-06-17 11:50:57 -0700

    Copy of RethinkDB

---
+++ README.md
@@ -0,0 +1,4 @@
+Ruby RethinkDB Sample
+=====================
+
+Uses RethinkDB to test simple document insertion on a CouchDB instance.
---
+++ bowling.rb
@@ -0,0 +1,28 @@
+require 'rethinkdb'
+include RethinkDB::Shortcuts
+
+class Bowling
+  @var = 0
+
+  def hit()
+    connection = r.connect(:host => 'localhost', :port => 28015).repl
+    r.db_drop('test').run
+    r.db_create('test').run
+
+    r.db('test').table_create('table').run
+
+    r.table("table").insert({
+        :id => 1,
+        :title => "Lorem ipsum",
+        :content => "Dolor sit amet"
+    }).run
+
+    puts r.table('table').get(1).keys.run.length
+    @var = r.table('table').get(1).keys.run.length
+  end
+
+  def score
+  	return @var
+  end
+end
+
